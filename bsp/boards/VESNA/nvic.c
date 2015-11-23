ca953ca6a100e76684be433ccdbf497a61dc5ba7
BostjanM <bostjan.mikelj@gmail.com>
2015-11-23 12:24:08 +0100

    Changed priority of SPI interrupt. Removed define condition.

--- bsp/boards/VESNA/nvic.c
+++ bsp/boards/VESNA/nvic.c
@@ -38,15 +38,13 @@ void NVIC_uart(void)
 //configuration spi interrput
 void NVIC_spi(void)
 {
-#ifdef SPI_IN_INTERRUPT_MODE
   //Configure NVIC: Preemption Priority = 1 and Sub Priority = 1
   NVIC_InitTypeDef NVIC_InitStructure;
   NVIC_InitStructure.NVIC_IRQChannel	                   = SPI2_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority   = 1;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority   = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority	         = 1;
   NVIC_InitStructure.NVIC_IRQChannelCmd	                 = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
-#endif
 }
 
 //configuration bsptimer interrput
