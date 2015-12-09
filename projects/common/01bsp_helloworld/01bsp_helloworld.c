3a0d7ac97db56163c7bb76ca728ea90975bdef60
BostjanM <bostjan.mikelj@gmail.com>
2015-12-09 12:14:04 +0100

    bsp_helloworld.c app now goes to sleep for 10sec before writing hello world!!! msg to USART1 again. App uses rtc timer to wakeup from sleep mode.

--- projects/common/01bsp_helloworld/01bsp_helloworld.c
+++ projects/common/01bsp_helloworld/01bsp_helloworld.c
@@ -7,21 +7,32 @@
 #include "string.h"
 #include "uart.h"
 #include "board.h"
+#include "rtc_timer.h"
+#include "leds.h"
 
 const char debugStr[] = "Hello World!!!\n";
 
+#define ALARM_PERIOD	327680	//10 seconds
+
+void alarm_cb(void);
 
 int mote_main(void){
 	uint8_t i;
 	// initialize board
 	board_init();
+	rtc_timer_init();
+	rtc_timer_setAlarmCb(alarm_cb);
+	rtc_timer_start(ALARM_PERIOD);
 	while(1){
 		for (i = 0;i < strlen(debugStr);i++){
 			uart_writeByte(debugStr[i]);
 		}
-		i = 0;
-		GPIO_ResetBits(GPIOB,GPIO_Pin_12);
-		board_timeDelayMS(5000);
-		GPIO_SetBits(GPIOB,GPIO_Pin_12);
+		board_timeDelayMS(2000);
+	board_sleep();
 	}
+}
+
+void alarm_cb(void){
+	leds_error_toggle();
+	rtc_timer_resetCounter();
 }
