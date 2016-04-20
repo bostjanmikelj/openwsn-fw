0097c20b9e9ff9a5abcd3a1c91c491253434748f
BostjanM <bostjan.mikelj@gmail.com>
2016-04-20 15:23:04 +0200

    Changed how eui64 is calculated from cortexm3 unique id number.

--- bsp/boards/VESNA/eui64.c
+++ bsp/boards/VESNA/eui64.c
@@ -9,11 +9,16 @@
 
 //=========================== defines =========================================
 // stm32f103rey, 96-bit unique ID address
-#define UNIQUE_ID_BASE_ADDRESS          0x1FFFF7E8	//Unique ID of proccessor used as MAC???
-
+#define UNIQUE_ID_BASE_ADDRESS          0x1FFFF7E8	//Unique ID first 8 bytes
+#define UNIQUE_ID_LAST_ADDRESS          0x1FFFF7EC	//Unique ID last 8 bytes
 //=========================== variables =======================================
-
+/* Uncomment only one of the following*/
+/*MAC address as proccessor ID stored in flash*/
 const uint8_t const *uid = (const uint8_t *const) UNIQUE_ID_BASE_ADDRESS;
+const uint8_t const *luid = (const uint8_t *const) UNIQUE_ID_LAST_ADDRESS;
+
+///* MAC address of node provided by user */
+//const uint8_t uid[8] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x04};
 
 //=========================== prototypes ======================================
 
@@ -21,14 +26,14 @@ const uint8_t const *uid = (const uint8_t *const) UNIQUE_ID_BASE_ADDRESS;
 
 void eui64_get(uint8_t* addressToWrite)
 {
-  addressToWrite[0] = uid[0];
-  addressToWrite[1] = uid[1];
-  addressToWrite[2] = uid[2];
-  addressToWrite[3] = uid[3];
-  addressToWrite[4] = uid[4];
-  addressToWrite[5] = uid[5];
-  addressToWrite[6] = uid[6];
-  addressToWrite[7] = uid[7];
+  addressToWrite[0] = (uid[0]^luid[0]);
+  addressToWrite[1] = (uid[1]^luid[2]);
+  addressToWrite[2] = (uid[2]^luid[4]);
+  addressToWrite[3] = (uid[3]^luid[6]);
+  addressToWrite[4] = (uid[4]^luid[1]);
+  addressToWrite[5] = (uid[5]^luid[3]);
+  addressToWrite[6] = (uid[6]^luid[5]);
+  addressToWrite[7] = (uid[7]^luid[7]);
 }
 
 //=========================== private =========================================
